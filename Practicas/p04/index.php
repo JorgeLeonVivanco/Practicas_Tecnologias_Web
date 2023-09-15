5fd1acc7c3fa28157d9a65a1680032d28d33b9ee
JorgeVivanco <jorge.leonvilumno.buap.mx>
2023-09-15 15:33:20 -0600

    Practica 4/Ejercicio 4 Terminado

--- Practicas/p04/index.php
+++ Practicas/p04/index.php
@@ -116,5 +116,32 @@
     echo "El primer número entero aleatorio múltiplo de $numero_dado es: $numero_aleatorio";
     ?>
 
+
+    <h2>Ejercicio 4: Crear un arreglo cuyos índices van de 97 a 122 y cuyos valores son las letras de la ‘a’
+    a la ‘z’. Usa la función chr(n) que devuelve el caracter cuyo código ASCII es n para poner
+    el valor en cada índice.</h2>
+    <p><h2>Crea el arreglo con un ciclo for</h2></p>
+    <p><h2>Lee el arreglo y crea una tabla en XHTML con echo y un ciclo foreach</h2></p>
+
+    <?php
+    // Crear el arreglo con un ciclo for
+    $arreglo = [];
+    for ($codigo = 97; $codigo <= 122; $codigo++) {
+        $letra = chr($codigo);
+        $arreglo[$codigo] = $letra;
+    }
+
+    // Generar la tabla XHTML con un ciclo foreach
+    echo "<table border='1'>";
+    echo "<tr><th>Índice</th><th>Valor</th></tr>";
+    foreach ($arreglo as $indice => $valor) {
+        echo "<tr>";
+        echo "<td>[$indice]</td>";
+        echo "<td>$valor</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    ?>
+
 </body>
 </html>

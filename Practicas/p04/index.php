8e5bfa42cd0e6cef0bd2f5512583d2bb325f68c7
JorgeVivanco <jorge.leonvilumno.buap.mx>
2023-09-15 14:29:02 -0600

    Practica 4/Ejercicio 2 Terminado

--- Practicas/p04/index.php
+++ Practicas/p04/index.php
@@ -21,5 +21,48 @@
         }
     ?>
 
+<h2>Ejercicio 2:Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por: impar,par,impar.</h2>
+    <?php
+    function generarNumeroAleatorio() 
+    {
+        return rand(1, 1000); //Función para generar un número aleatori y el rango
+    }
+    function esSecuenciaValida($secuencia) 
+    {
+        return $secuencia[0] % 2 != 0 && $secuencia[1] % 2 == 0 && $secuencia[2] % 2 != 0;
+    }    // Función para verificar si la secuencia es impar, par, impar
+    $matriz = [];
+    $iteraciones = 0;
+    $numerosGenerados = 0;
+    while (true) 
+    {  // Bucle para generar la matriz
+        $numerosGenerados++;
+        $secuencia = [generarNumeroAleatorio(), generarNumeroAleatorio(), generarNumeroAleatorio()];
+        if (esSecuenciaValida($secuencia))
+        {
+            $matriz[] = $secuencia;
+            $iteraciones++;
+            if ($iteraciones == 4) // Cambia el número de iteraciones deseado
+                { 
+                    break;
+                }
+        }
+    }
+    // Mostrar la matriz como una tabla
+    echo "<table border='1'>";
+    foreach ($matriz as $fila) 
+    {
+        echo "<tr>";
+        foreach ($fila as $valor) 
+        {
+            echo "<td>$valor</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "$numerosGenerados números generados en $iteraciones iteraciones";
+    ?>
+
 </body>
 </html>
